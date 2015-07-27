ffcd129949762764139dcbdb7bbac5e8732a129f
kenesyu <5888101@qq.com>
2015-07-27 18:40:21 +0800

    111

---
+++ WebAPP/WebApp/WXPay/Products.aspx
@@ -0,0 +1,105 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Products.aspx.cs" Inherits="WebAPP.WebApp.WXPay.Products" %>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Mosaddek">
+    <meta name="keyword" content="FlatLab, Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+    <link rel="shortcut icon" href="img/favicon.png">
+
+    <title>在线支付</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../css/bootstrap-reset.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="../../css/style.css" rel="stylesheet">
+    <link href="../../css/style-responsive.css" rel="stylesheet" />
+    <link rel="stylesheet" href="../../css/form.css"/>
+
+    <script language="javascript" src="http://res.mail.qq.com/mmr/static/lib/js/jquery.js" type="text/javascript"></script>
+    <script language="javascript" src="http://res.mail.qq.com/mmr/static/lib/js/lazyloadv3.js" type="text/javascript"></script>
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+<!--main content start-->
+<section id="main-content">
+    <section class="wrapper">
+        <!-- page start-->
+        <div class="row">
+            <div class="col-lg-12">
+                <section class="panel">
+                    <header class="panel-heading">
+                        商品信息
+                    </header>
+                    <table class="table">
+
+                        <tbody>
+                        <tr>
+                            <td>商品名称：</td>
+                            <td><asp:Label ID="lblProductName" runat="server"></asp:Label></td>
+                        </tr>
+                        <tr>
+                            <td>优惠价：</td>
+                            <td><asp:Label ID="lblPrice" runat="server"></asp:Label></td>
+                        </tr>
+                        <tr>
+                            <td>使用说明：</td>
+                            <td><asp:Label ID="lblDetails" runat="server"></asp:Label></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </section>
+            </div>
+        </div>
+        <!-- page end-->
+    </section>
+</section>
+<!--main content end-->
+<div>
+    <%= WeiPay.PayConfig.AppId %> </br>
+    <%= TimeStamp %></br>
+    <%= NonceStr %></br>
+    <%= Package %></br>
+    <%= PaySign %>
+</div>
+<div class="ui-btn pay-btn" onclick="SavePay()">立即购买</div>
+
+<script type="text/javascript">
+
+    function SavePay() {
+        WeixinJSBridge.invoke('getBrandWCPayRequest', {
+            "appId": "<%= WeiPay.PayConfig.AppId %>", //公众号名称，由商户传入
+            "timeStamp": "<%= TimeStamp %>", //时间戳
+            "nonceStr": "<%= NonceStr %>", //随机串
+            "package": "<%= Package %>", //扩展包
+            "signType": "MD5", //微信签名方式:1.sha1
+            "paySign": "<%= PaySign %>" //微信签名
+        },
+               function (res) {
+                   if (res.err_msg == "get_brand_wcpay_request:ok") {
+                       alert("微信支付成功!");
+                   } else if (res.err_msg == "get_brand_wcpay_request:cancel") {
+                       alert("用户取消支付!");
+                   } else {
+                       alert(res.err_msg);
+                       alert("支付失败!");
+                   }
+                   // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
+                   //因此微信团队建议，当收到ok返回时，向商户后台询问是否收到交易成功的通知，若收到通知，前端展示交易成功的界面；若此时未收到通知，商户后台主动调用查询订单接口，查询订单的当前状态，并反馈给前端展示相应的界面。
+               });
+    }
+   
+
+
+</script>
+</body>
+</html>
---
+++ WebAPP/WebApp/WXPay/Products.aspx.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DBHelper;
+using System.Configuration;
+using System.Data;
+using WeiPay;
+using Newtonsoft.Json;
+using System.Xml;
+
+namespace WebAPP.WebApp.WXPay
+{
+    public partial class Products : System.Web.UI.Page
+    {
+        //页面输出 不用操作
+        public static string Code = "";     //微信端传来的code
+        public static string PrepayId = ""; //预支付ID
+        public static string Sign = "";     //为了获取预支付ID的签名
+        public static string PaySign = "";  //进行支付需要的签名
+        public static string Package = "";  //进行支付需要的包
+        public static string TimeStamp = ""; //时间戳 程序生成 无需填写
+        public static string NonceStr = ""; //随机字符串  程序生成 无需填写
+
+
+        //支付相关参数 ，以下参数请从需要支付的页面通过get方式传递过来
+        protected string OrderSN = ""; //商户自己订单号
+        protected string Body = ""; //商品描述
+        protected string TotalFee = "";  //总支付金额，单位为：分，不能有小数
+        protected string Attach = ""; //用户自定义参数，原样返回
+        protected string UserOpenId = "";//微信用户openid
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) {
+                BindData();
+            }
+        }
+
+        private void BindData() {
+            if (Request.QueryString["ID"] != null && Request.QueryString["ID"].ToString() != "")
+            {
+                DataBaseHelper dbHelper = new DataBaseHelper(ConfigurationManager.ConnectionStrings["DB"].ToString());
+                DataTable dt = dbHelper.ExecuteDataTable("select * from T_Products where productid = " + Request.QueryString["id"].ToString() + " and isopen = 1");
+                if (dt.Rows.Count > 0)
+                {
+                    this.lblProductName.Text = dt.Rows[0]["ProductName"].ToString();
+                    this.lblPrice.Text = dt.Rows[0]["ProductPrice"].ToString();
+                    this.lblDetails.Text = dt.Rows[0]["Details"].ToString();
+                    this.TotalFee = (Convert.ToDecimal(dt.Rows[0]["ProductPrice"]) * 100).ToString();
+                    dbHelper.Dispose();
+
+
+                    GetUserOpenId();
+                    LogUtil.WriteLog("============ 单次支付开始 ===============");
+                    LogUtil.WriteLog(string.Format("传递支付参数：OrderSN={0}、Body={1}、TotalFee={2}、Attach={3}、UserOpenId={4}",
+                    this.OrderSN, this.Body, this.TotalFee, this.Attach, this.UserOpenId));
+
+
+                    #region 支付操作============================
+
+
+                    #region 基本参数===========================
+                    //时间戳 
+                    TimeStamp = TenpayUtil.getTimestamp();
+                    //随机字符串 
+                    NonceStr = TenpayUtil.getNoncestr();
+
+                    //创建支付应答对象
+                    var packageReqHandler = new RequestHandler(Context);
+                    //初始化
+                    packageReqHandler.init();
+
+                    //设置package订单参数  具体参数列表请参考官方pdf文档，请勿随意设置
+                    packageReqHandler.setParameter("body", this.Body); //商品信息 127字符
+                    packageReqHandler.setParameter("appid", PayConfig.AppId);
+                    packageReqHandler.setParameter("mch_id", PayConfig.MchId);
+                    packageReqHandler.setParameter("nonce_str", NonceStr.ToLower());
+                    packageReqHandler.setParameter("notify_url", PayConfig.NotifyUrl);
+                    packageReqHandler.setParameter("openid", this.UserOpenId);
+                    packageReqHandler.setParameter("out_trade_no", this.OrderSN); //商家订单号
+                    packageReqHandler.setParameter("spbill_create_ip", Page.Request.UserHostAddress); //用户的公网ip，不是商户服务器IP
+                    packageReqHandler.setParameter("total_fee", this.TotalFee); //商品金额,以分为单位(money * 100).ToString()
+                    packageReqHandler.setParameter("trade_type", "JSAPI");
+                    if (!string.IsNullOrEmpty(this.Attach))
+                        packageReqHandler.setParameter("attach", "test");//自定义参数 127字符
+
+                    #endregion
+
+                    #region sign===============================
+                    Sign = packageReqHandler.CreateMd5Sign("key", PayConfig.AppKey);
+                    LogUtil.WriteLog("WeiPay 页面  sign：" + Sign);
+                    #endregion
+
+                    #region 获取package包======================
+                    packageReqHandler.setParameter("sign", Sign);
+
+                    string data = packageReqHandler.parseXML();
+                    LogUtil.WriteLog("WeiPay 页面  package（XML）：" + data);
+
+                    string prepayXml = HttpUtil.Send(data, "https://api.mch.weixin.qq.com/pay/unifiedorder");
+                    LogUtil.WriteLog("WeiPay 页面  package（Back_XML）：" + prepayXml);
+
+                    //获取预支付ID
+                    var xdoc = new XmlDocument();
+                    xdoc.LoadXml(prepayXml);
+                    XmlNode xn = xdoc.SelectSingleNode("xml");
+                    XmlNodeList xnl = xn.ChildNodes;
+                    if (xnl.Count > 7)
+                    {
+                        PrepayId = xnl[7].InnerText;
+                        Package = string.Format("prepay_id={0}", PrepayId);
+                        LogUtil.WriteLog("WeiPay 页面  package：" + Package);
+                    }
+                    #endregion
+
+                    #region 设置支付参数 输出页面  该部分参数请勿随意修改 ==============
+                    var paySignReqHandler = new RequestHandler(Context);
+                    paySignReqHandler.setParameter("appId", PayConfig.AppId);
+                    paySignReqHandler.setParameter("timeStamp", TimeStamp);
+                    paySignReqHandler.setParameter("nonceStr", NonceStr);
+                    paySignReqHandler.setParameter("package", Package);
+                    paySignReqHandler.setParameter("signType", "MD5");
+                    PaySign = paySignReqHandler.CreateMd5Sign("key", PayConfig.AppKey);
+
+                    LogUtil.WriteLog("WeiPay 页面  paySign：" + PaySign);
+                    #endregion
+                    #endregion
+                }
+                else
+                {
+                    Response.Write("<font size='24px'>该商品已销售完</font>");
+                    Response.End();
+                }
+            }
+            else
+            {
+                Response.Write("<font size='24px'>该商品已销售完</font>");
+                Response.End();
+            }
+        }
+
+        private void GetUserOpenId()
+        {
+            string code = Request.QueryString["code"];
+            if (string.IsNullOrEmpty(code))
+            {
+                string code_url = string.Format("https://open.weixin.qq.com/connect/oauth2/authorize?appid={0}&redirect_uri={1}&response_type=code&scope=snsapi_base&state=lk#wechat_redirect", PayConfig.AppId, Request.Url.ToString());
+                Response.Redirect(code_url);
+            }
+            else
+            {
+                LogUtil.WriteLog(" ============ 开始 获取微信用户相关信息 =====================");
+
+                #region 获取支付用户 OpenID================
+                string url = string.Format("https://api.weixin.qq.com/sns/oauth2/access_token?appid={0}&secret={1}&code={2}&grant_type=authorization_code", PayConfig.AppId, PayConfig.AppSecret, code);
+                string returnStr = HttpUtil.Send("", url);
+                LogUtil.WriteLog("Send 页面  returnStr 第一个：" + returnStr);
+
+                var obj = JsonConvert.DeserializeObject<OpenModel>(returnStr);
+
+                url = string.Format("https://api.weixin.qq.com/sns/oauth2/refresh_token?appid={0}&grant_type=refresh_token&refresh_token={1}", PayConfig.AppId, obj.refresh_token);
+                returnStr = HttpUtil.Send("", url);
+                obj = JsonConvert.DeserializeObject<OpenModel>(returnStr);
+
+                LogUtil.WriteLog("Send 页面  access_token：" + obj.access_token);
+                LogUtil.WriteLog("Send 页面  openid=" + obj.openid);
+
+                url = string.Format("https://api.weixin.qq.com/sns/userinfo?access_token={0}&openid={1}", obj.access_token, obj.openid);
+                returnStr = HttpUtil.Send("", url);
+                LogUtil.WriteLog("Send 页面  returnStr：" + returnStr);
+
+                this.UserOpenId = obj.openid;
+
+                LogUtil.WriteLog(" ============ 结束 获取微信用户相关信息 =====================");
+                #endregion
+            }
+        }
+    }
+}
